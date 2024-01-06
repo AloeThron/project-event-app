958c37d39a83c0a43c44fc2d5e3542455c282ab5
AloeThron <lxe.tr.deat@gmail.com>
2024-01-06 18:11:54 +0700

    setup auth connDB

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+type props = {
+  children: React.ReactNode
+}
+
+const Layout = ({ children }: props) => {
+  return (
+    <div className="flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
